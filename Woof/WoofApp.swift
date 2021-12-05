7270e831477f73c50883ce0a332d142994f4d256
Mike JS. Choi <mkchoi212@icloud.com>
2021-12-05 20:56:38 +0900

    Initial Commit

---
+++ Woof/WoofApp.swift
@@ -0,0 +1,17 @@
+//
+//  WoofApp.swift
+//  Woof
+//
+//  Created by Mike Choi on 12/5/21.
+//
+
+import SwiftUI
+
+@main
+struct WoofApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
